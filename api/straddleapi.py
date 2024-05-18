e017d1615564ab44fa591cf7a680891c301ed4c1
sawantajay22 <sawant.s.ajay@gmail.com>
2024-05-18 22:41:50 +0530

    Add files via upload

--- api/straddleapi.py
+++ api/straddleapi.py
@@ -1,15 +1,38 @@
+import os
+import json
+from flask import Flask, request, jsonify
 
-from flask import Flask, request
-from service.straddleservice import StraddleService
 
-app = Flask(__name__)
-straddle_service = StraddleService()
+os.chdir('E:/Sagar_Strategy_API-main')
+print(f"Changed directory to: {os.getcwd()}")
 
-@app.route('/data', methods=['POST'])
+try:
+    from service.straddleservice import Strategyservice
+except ImportError as e:
+    print(f"Error importing StraddleService: {e}")
+
+# Initialize Flask app
+#app = Flask(__name__)
+
+with open('db.json', 'r') as f:
+    data = json.load(f)
+#print("Data from db.json:", data) 
+#return jsonify(data), 200
+strategy = Strategyservice(data)
+strategy.process_data(data)
+#print(strategy)
+
+'''
+@app.route('/get_data', methods=['POST'])
 def get_data():
-    data = request.json
-    straddle_service.process_data(data)
+    with open('db.json', 'r') as f:
+        data = json.load(f)
+    #print("Data from db.json:", data) 
+    return jsonify(data), 200
+    strategy_instance = Strategy()
+    strategy_instance.process_data(data)
+'''
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+#if __name__ == '__main__':
+
+    #app.run(debug=True)
